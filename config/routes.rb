7c94194f50d33b1e326b81fe51d729a10b2fdb53
Jim Boyd <jrboydjr.spfd@gmail.com>
2011-08-11 16:35:22 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 SampleApp::Application.routes.draw do
 
+  get "user/new"
+
+  match '/signup', :to => 'user#new'
+
+  root :to => 'pages#home'
+
   match '/contact', :to => 'pages#contact'
 
   match '/about', :to => 'pages#about'
 
   match '/help', :to => 'pages#help'
-
-  root :to => 'pages#home'
-  
-
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
